aa363b2176182410f8595b59e1793759132edf2c
SShlykov <zeitment@gmail.com>
2024-02-20 00:05:05 +0300

    feat: The server pings the database, if the MaxPingAttempts limit is exceeded, the server stops automatically(мÑvia context)

--- bookback/internal/config/pg.go
+++ bookback/internal/config/pg.go
@@ -3,6 +3,7 @@ package config
 import (
 	"errors"
 	"os"
+	"time"
 )
 
 const (
@@ -13,10 +14,14 @@ var ErrDSNNotSet = errors.New("DSN not set")
 
 type PGConfig interface {
 	DSN() string
+	MaxPingAttempts() int
+	PingInterval() time.Duration
 }
 
 type pgConfig struct {
-	dsn string
+	dsn             string
+	maxPingAttempts int
+	pingInterval    time.Duration
 }
 
 func NewPGConfig() (PGConfig, error) {
@@ -25,9 +30,11 @@ func NewPGConfig() (PGConfig, error) {
 		return nil, ErrDSNNotSet
 	}
 
-	return &pgConfig{dsn: dsn}, nil
+	return &pgConfig{dsn: dsn, maxPingAttempts: 10, pingInterval: 3 * time.Second}, nil
 }
 
 func (c *pgConfig) DSN() string {
 	return c.dsn
 }
+func (c *pgConfig) MaxPingAttempts() int        { return c.maxPingAttempts }
+func (c *pgConfig) PingInterval() time.Duration { return c.pingInterval }
--- bookback/internal/pkg/app/db.go
+++ bookback/internal/pkg/app/db.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"github.com/SShlykov/zeitment/bookback/internal/config"
 	"github.com/SShlykov/zeitment/bookback/pkg/db/pg"
+	"time"
 )
 
 func (a *App) initDB(ctx context.Context) error {
@@ -17,5 +18,25 @@ func (a *App) initDB(ctx context.Context) error {
 		return errors.New("failed to init pg client: " + err.Error())
 	}
 	a.db = db
+
+	go func() {
+		var broken int
+		ticker := time.NewTicker(pgConf.PingInterval())
+		for range ticker.C {
+			err = db.DB().Ping(ctx)
+			if err != nil {
+				broken++
+				if broken > pgConf.MaxPingAttempts() {
+					a.logger.Error("db is down")
+					a.closeCtx()
+					break
+				}
+				a.logger.Error("failed to ping db")
+			} else {
+				broken = 0
+			}
+		}
+	}()
+
 	return nil
 }
