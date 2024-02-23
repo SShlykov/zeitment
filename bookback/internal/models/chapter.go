14bba50ed1ef5c79c3c8af932288f7656137c5a4
SShlykov <zeitment@gmail.com>
2024-02-23 23:42:52 +0300

    feat: feilds

--- bookback/internal/models/chapter.go
+++ bookback/internal/models/chapter.go
@@ -1,19 +1,19 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 )
 
-// DeletedAt sql.NullTime `json:"deleted_at"`
-
 type Chapter struct {
-	ID        string    `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Title     string    `json:"title"`
-	Number    int       `json:"number"`
-	Text      string    `json:"text"`
-	BookID    string    `json:"book_id"`
-	IsPublic  bool      `json:"is_public"`
-	Pages     []Page    `json:"pages"`
+	ID        string       `json:"id"`
+	CreatedAt time.Time    `json:"created_at"`
+	UpdatedAt time.Time    `json:"updated_at"`
+	DeletedAt sql.NullTime `json:"deleted_at"`
+	Title     string       `json:"title"`
+	Number    int          `json:"number"`
+	Text      string       `json:"text"`
+	BookID    string       `json:"book_id"`
+	IsPublic  bool         `json:"is_public"`
+	Pages     []Page       `json:"pages"`
 }
--- bookback/internal/models/page.go
+++ bookback/internal/models/page.go
@@ -1,16 +1,17 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 )
 
 type Page struct {
-	ID        string    `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Text      string    `json:"text"`
-	ChapterID string    `json:"chapter_id"`
-	IsPublic  bool      `json:"is_public"`
+	ID        string       `json:"id"`
+	CreatedAt time.Time    `json:"created_at"`
+	UpdatedAt time.Time    `json:"updated_at"`
+	DeletedAt sql.NullTime `json:"deleted_at"`
+	Text      string       `json:"text"`
+	ChapterID string       `json:"chapter_id"`
+	IsPublic  bool         `json:"is_public"`
 	//Paragraphs []Paragraph
-	//DeletedAt sql.NullTime `json:"deleted_at"`
 }
