f7b25d24ff8d7869298434776ed86fa6a64b19c2
Vyachesalv <v.a.buglov@gmail.com>
2024-02-29 22:24:40 +0300

    feat(books): add books api service

---
+++ books-ui/src/store/modules/userBooks/index.js
@@ -0,0 +1,49 @@
+import menuList from "./menuList.js"
+
+const state = {
+  height:      0,
+  width:       0,
+  inited:      false,
+  isOpenMenu:  true
+};
+
+const getters = {
+  height:                  (state) => state.height,
+  width:                   (state) => state.width,
+  isOpenMenu:              (state) => state.isOpenMenu,
+  menuWidth:               (state) => state.isOpenMenu ? 300 : 70,
+  menuList:                ()      => menuList
+};
+
+const mutations = {
+  enableScreenListener: (state) => {
+    state.height = window.innerHeight
+        state.width = window.innerWidth
+    const acc = !!state.inited
+    if (!acc) {
+      window.addEventListener("resize", (e) => {
+        state.height= e.target.innerHeight
+        state.width = e.target.innerWidth
+      });
+      state.inited = true
+    }
+  },
+  toggleMenu: (state) => {
+    state.isOpenMenu = !state.isOpenMenu
+  }
+};
+
+const actions = {
+  initScreenSizeRecalc: ({commit}) => {
+    commit("enableScreenListener")
+  },
+
+};
+
+export const store = {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
--- books-ui/src/tests/helpers/adapter.test.js
+++ books-ui/src/tests/helpers/adapter.test.js
@@ -1,6 +1,5 @@
 import { expect, test } from 'vitest'
-import { convertObject, covertList } from './adapter'
-
+import { convertObject, convertList } from '@helpers/server-routes/adapter/adapter.js'
 
 test('parse object from api', () => {
   const targetObject = {
@@ -17,26 +16,29 @@ test('parse object from api', () => {
     "is_online": true
   }
 
-  const selfObject = {
-    "created_at": "2024-02-15T17:24:52.755254148+03:00",
-    "description": "localhost",
-    "device_ip": "172.25.78.36",
-    "proxy_ip": "172.25.78.153",
-    "is_online": true,
-    "id": 1,
-    "port2": 9965,
-    "name": "jsonplaceholder.typicode.com",
-    "internet_uri": "localhost",
+  const expectedObject = {
+    id: 1,
+    name2: "jsonplaceholder.typicode.com",
+    port2: 9965,
+    proxy_ip: "172.25.78.153",
+    site_ip: "172.25.78.36",
+    internet_uri: "localhost",
+    description: "localhost",
+    is_online: true,
   }
 
   const config = {
+    id: "id",
+    name: "name2",
     port: "port2",
-    site_ip: "device_ip"
+    proxy_ip: "proxy_ip",
+    site_ip: "site_ip",
+    internet_uri: "internet_uri",
+    description: "description",
+    is_online: "is_online",
   }
 
-  const listExcluded = ['updated_at', 'updated_at', 'deleted_at']
-
-  expect(convertObject(targetObject, config, {listExcluded})).toEqual(selfObject)
+  expect(convertObject(targetObject, {config})).toEqual(expectedObject)
 })
 
 test('adapt list objects without callback', () => {
@@ -83,49 +85,50 @@ test('adapt list objects without callback', () => {
   const targetList = [targetObject1, targetObject2, targetObject3]
 
   const selfObject1 = {
-    "created_at": "2024-02-15T17:24:52.755254148+03:00",
-    "description": "localhost",
-    "device_ip": "172.25.78.36",
-    "proxy_ip": "172.25.78.153",
-    "is_online": true,
-    "id": 1,
-    "port2": 9965,
-    "name": "jsonplaceholder.typicode.com",
-    "internet_uri": "localhost",
+    id: 1,
+    name2: "jsonplaceholder.typicode.com",
+    port2: 9965,
+    proxy_ip: "172.25.78.153",
+    site_ip: "172.25.78.36",
+    internet_uri: "localhost",
+    description: "localhost",
+    is_online: true,
   }
   const selfObject2 = {
-    "created_at": "2024-02-15T17:24:52.755254148+03:00",
-    "description": "localhost",
-    "device_ip": "172.25.78.36",
-    "proxy_ip": "172.25.78.153",
-    "is_online": true,
-    "id": 1,
-    "port2": 9965,
-    "name": "jsonplaceholder.typicode.com",
-    "internet_uri": "localhost",
+    id: 1,
+    name2: "jsonplaceholder.typicode.com",
+    port2: 9965,
+    proxy_ip: "172.25.78.153",
+    site_ip: "172.25.78.36",
+    internet_uri: "localhost",
+    description: "localhost",
+    is_online: true,
   }
   const selfObject3 = {
-    "created_at": "2024-02-15T17:24:52.755254148+03:00",
-    "description": "localhost",
-    "device_ip": "172.25.78.36",
-    "proxy_ip": "172.25.78.153",
-    "is_online": true,
-    "id": 1,
-    "port2": 9965,
-    "name": "jsonplaceholder.typicode.com",
-    "internet_uri": "localhost",
+    id: 1,
+    name2: "jsonplaceholder.typicode.com",
+    port2: 9965,
+    proxy_ip: "172.25.78.153",
+    site_ip: "172.25.78.36",
+    internet_uri: "localhost",
+    description: "localhost",
+    is_online: true,
   }
 
   const selfList = [selfObject1, selfObject2, selfObject3]
 
   const config = {
+    id: "id",
+    name: "name2",
     port: "port2",
-    site_ip: "device_ip"
+    proxy_ip: "proxy_ip",
+    site_ip: "site_ip",
+    internet_uri: "internet_uri",
+    description: "description",
+    is_online: "is_online",
   }
 
-  const listExcluded = ['updated_at', 'updated_at', 'deleted_at']
-
-  expect(covertList(targetList, config, {listExcluded})).toEqual(selfList)
+  expect(convertList(targetList, {config})).toEqual(selfList)
 })
 
 test('adapt list objects with callback', () => {
@@ -158,40 +161,42 @@ test('adapt list objects with callback', () => {
 
   const targetList = [targetObject1, targetObject2]
 
-  const selfObject1 = {
-    "created_at": "2024-02-15T17:24:52.755254148+03:00",
-    "description": "localhost",
-    "device_ip": "172.25.78.36",
-    "proxy_ip": "172.25.78.153",
-    "state": "active",
-    "id": 1,
-    "port2": 9965,
-    "name": "jsonplaceholder.typicode.com",
-    "internet_uri": "localhost",
+  const expectedObject1 = {
+    id: 1,
+    name2: "jsonplaceholder.typicode.com",
+    port2: 9965,
+    proxy_ip: "172.25.78.153",
+    site_ip: "172.25.78.36",
+    internet_uri: "localhost",
+    description: "localhost",
+    state: "active",
   }
-  const selfObject2 = {
-    "created_at": "2024-02-15T17:24:52.755254148+03:00",
-    "description": "localhost",
-    "device_ip": "172.25.78.36",
-    "proxy_ip": "172.25.78.153",
-    "state": "disabled",
-    "id": 1,
-    "port2": 9965,
-    "name": "jsonplaceholder.typicode.com",
-    "internet_uri": "localhost",
+
+  const expectedObject2 = {
+    id: 1,
+    name2: "jsonplaceholder.typicode.com",
+    port2: 9965,
+    proxy_ip: "172.25.78.153",
+    site_ip: "172.25.78.36",
+    internet_uri: "localhost",
+    description: "localhost",
+    state: "disabled",
   }
 
 
-  const selfList = [selfObject1, selfObject2]
+  const expectedList = [expectedObject1, expectedObject2]
 
   const config = {
+    id: "id",
+    name: "name2",
     port: "port2",
-    site_ip: "device_ip"
+    proxy_ip: "proxy_ip",
+    site_ip: "site_ip",
+    internet_uri: "internet_uri",
+    description: "description",
   }
-
-  const listExcluded = ['updated_at', 'updated_at', 'deleted_at', 'is_online']
 
   const callback = (el, {is_online}) => ({...el, state: is_online ? "active": "disabled"})
 
-  expect(covertList(targetList, config, {listExcluded, callback})).toEqual(selfList)
+  expect(convertList(targetList, {config, callback})).toEqual(expectedList)
 })
